6e3d058bf16a2c6599744c55158627f4f09093a9
Sergio <norgestream@gmail.com>
2024-11-16 18:17:46 +0200

    fetched and display posts from jsonplaceholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { get } from './util/http.ts';
+import { ReactNode, useEffect, useState } from 'react';
+import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPosts = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get("https://jsonplaceholder.typicode.com/posts/")) as RawDataBlogPosts[];
+
+      const blogPosts: BlogPost[] = data.map(post => {
+        return {
+          id: post.id,
+          title: post.title,
+          text: post.body
+        }
+      })
+      setFetchedPosts(blogPosts)
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts = {fetchedPosts} />
+  }
+
+
+  return <main>
+    <img src={fetchingImg} alt="An image depicting a fetching process" />
+    {content}
+  </main>;
 }
 
 export default App;
