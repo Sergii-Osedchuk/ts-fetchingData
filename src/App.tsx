6486b46670abe7337d6d9a89979af28c6532ee5d
Sergio <norgestream@gmail.com>
2024-11-18 14:33:18 +0200

    added error handling and loading while fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { get } from './util/http.ts';
 import { ReactNode, useEffect, useState } from 'react';
 import BlogPosts, { BlogPost } from './components/BlogPosts.tsx';
 import fetchingImg from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage.tsx';
 
 type RawDataBlogPosts = {
   id: number;
@@ -12,19 +13,28 @@ type RawDataBlogPosts = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get("https://jsonplaceholder.typicode.com/posts/")) as RawDataBlogPosts[];
-
-      const blogPosts: BlogPost[] = data.map(post => {
-        return {
-          id: post.id,
-          title: post.title,
-          text: post.body
-        }
-      })
-      setFetchedPosts(blogPosts)
+      setIsFetching(true);
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts/"
+        )) as RawDataBlogPosts[];
+        const blogPosts: BlogPost[] = data.map(post => {
+          return {
+            id: post.id,
+            title: post.title,
+            text: post.body
+          }
+        });
+        setFetchedPosts(blogPosts)
+      } catch (error) {
+        setError("Failed to fetch post")
+      }
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -32,8 +42,16 @@ function App() {
 
   let content: ReactNode;
 
+  if (error) {
+    content = <ErrorMessage text={error} />
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts = {fetchedPosts} />
+  }
+
+  if (isFetching) {
+    content = <p id='loading-fallback'>Fetching posts...</p>
   }
 
 
